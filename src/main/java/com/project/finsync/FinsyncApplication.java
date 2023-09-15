ac430d58546a8faac6238b12a41c888f114b0bc5
Ammar Y. Awad <ammaryasser085@gmail.com>
2023-09-15 17:03:37 +0300

    Add the generated spring boot code for the back-end side.

---
+++ src/main/java/com/project/finsync/FinsyncApplication.java
@@ -0,0 +1,13 @@
+package com.project.finsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/finsync/FinsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.finsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
